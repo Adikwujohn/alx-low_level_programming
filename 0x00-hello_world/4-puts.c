ec1e45096939a4066e0de65667b2fa3e10bdf6a1
AdikwuJohn <johnaudu496@gmail.com>
2023-12-14 15:17:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success) 
+ */
+  int main ()
+      {
+	      puts("\"Programming is like building a multilingual puzzle\n");
+	      return 0;
+      }
