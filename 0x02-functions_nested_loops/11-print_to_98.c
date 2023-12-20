2cbe2fbe7261f861fe64b61780612172f975696c
AdikwuJohn <johnaudu496@gmail.com>
2023-12-20 15:05:43 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,15 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: The number to start printing from.
+ * prints numbers separated by a comma followed by a space from n to 98.
+ *
+ * in ascending or descending order,followed by a new line.
  */
 void print_to_98(int n)
 {
 while (n != 98)
 {
-printf("%d, ",n);
+printf("%d, ", n);
 if (n > 98)
 {
 n--;
@@ -18,7 +21,7 @@ else
 n++;
 }
 }
-printf("%d\n" ,n);
+printf("%d\n", n);
 /**
  * prints the last number(98) with a new line
  */
