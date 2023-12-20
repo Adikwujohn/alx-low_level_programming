6f5687757d298292a2c0d3e25449c0cbda99df3e
AdikwuJohn <johnaudu496@gmail.com>
2023-12-20 15:54:46 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
+
 /**
- * code: prints the first 98 fibonacci numbers, starting with 1 and 2, followed by a new line
- * Return: always 0 (success)
+ * Prints the first 98 Fibonacci numbers, starting with 1 and 2, separated by commas and followed by a new line.
+ * Returns: Always 0 (success)
  */
-void print_fibonacci()
-{
-int a = 1, b = 2, count = 0;
-while (count < 98)
-{
-int next = a+b;
-printf("%d", next);
-if (count != 98)
-printf(",");
-}
-a = b;
-b = next;
-count++;
-}
-printf("\n");
-}
-int main()
-{
-print_fibonacci();
-return (0);
+int main() {
+    int a = 1, b = 2, count = 0;
+
+    while (count < 98) {
+        int next = a + b;
+        printf("%d", next);
+
+        if (count != 97) { // Ensures no comma after the last number
+            printf(", ");
+        }
+
+        a = b;
+        b = next;
+        count++;
+    }
+
+    printf("\n");
+    return 0;
 }
