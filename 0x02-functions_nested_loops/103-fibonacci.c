e3cb6b154db83236b6bd7b2549ecbc59898aa7c8
AdikwuJohn <johnaudu496@gmail.com>
2023-12-20 16:12:17 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,12 +2,12 @@
 
 int main() {
     int a = 1, b = 2, sum = 0;
-
+    int next;
     while (b <= 4000000) {
         if (b % 2 == 0) {  
             sum += b; 
         }
-        int next = a + b;
+        next = a + b;
         a = b;
         b = next;
     }
