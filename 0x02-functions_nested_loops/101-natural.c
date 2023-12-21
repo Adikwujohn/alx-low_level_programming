caf3b90e68dc0eb8bd1fdf04d69b1fef966dc63d
AdikwuJohn <johnaudu496@gmail.com>
2023-12-20 16:28:10 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,13 +3,15 @@
 int main() {
     int limit = 1024;
     int sum = 0;
+    
+    int i; // Declare 'i' here
 
-    for (int i = 1; i < limit; i++) {
+    for (i = 1; i < limit; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
     }
-
+    
     printf("%d\n", sum);
     return 0;
 }
