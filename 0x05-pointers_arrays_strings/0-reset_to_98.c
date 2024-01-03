eafa591093680033ec7fe5e1fe1325854c0407f7
AdikwuJohn <johnaudu496@gmail.com>
2024-01-02 16:16:09 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+/*reset_to_98 - resets the integer value*/
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+/**
+ * function prototype
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+
+#endif /* MAIN_H */
