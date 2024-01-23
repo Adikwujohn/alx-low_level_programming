de3ec9999b7db4bce00255d123691e3d56d7d8ce
AdikwuJohn <johnaudu496@gmail.com>
2024-01-23 06:26:23 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines new type struct dog
+ * @name: first member
+ * @age: second member.
+ * @owner: third member
+ * struct dog - structure dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
