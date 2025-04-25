aa36ef3402bf2b121d9f3cbfe9fa1ce3203a4766
Adikwujohn <johnaudu496@gmail.com>
2025-04-25 12:58:35 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: this function prints a message to standard out
+ * followed by a newline it serves as the starting point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
