e816c71b0f64a47d73bd5ceed2476b76b7aa8d56
Adikwujohn <johnaudu496@gmail.com>
2025-04-25 05:14:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description - this message prints a message to the standard output .
+ * followed by a new line, it serves as the starting point of the program.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
