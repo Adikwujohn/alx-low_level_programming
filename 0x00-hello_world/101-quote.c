b3f6b7be100a1fea55ee99bb0cd77d0d0ab869bb
Adikwujohn <johnaudu496@gmail.com>
2025-05-01 02:38:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (As required)
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 68);
+	return (1);
+}
